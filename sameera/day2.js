61b62ec9775033b9b67f91ac74f8612fff2677b9
raffid-nr <raffi.d@nathanresearchcom>
2026-05-19 15:32:36 +0530

    added day3

---
+++ sameera/day2.js
@@ -0,0 +1,25 @@
+// numbers
+let age = 45;
+let price = 12.98;
+let gpa = 6.9;
+
+console.log(`you are ${age} years old`);
+console.log(`the price is $${price}`);
+console.log(`your gpa is: ${gpa}`);
+
+//strings
+let firstname = "SAMEERA";
+let favouritefood = "pizza";
+let email = "sameerad711@gmail.com";
+
+console.log(`my name is ${firstname}`);
+console.log(`my favourite food is ${favouritefood}`);
+console.log(`my email id is ${email}`);
+
+//boolean
+let online = true;
+let studentid = false;
+
+console.log(`sameeera is online:${online}`);
+console.log(`enrolled:${studentid}`);
+
