595d74ff775818af422fb575af87e4fcef07240b
varsha <vijayasrivarshavutla@gmail.com>
2026-05-22 17:52:43 +0530

    added day2

---
+++ vijayasrivarsha/node.js/day2.js
@@ -0,0 +1,70 @@
+//Built-in modules
+//fs
+const fs = require('fs');
+
+// Write a file
+fs.writeFileSync('example.txt', 'Hello World');
+
+// Read a file
+const data = fs.readFileSync('example.txt', 'utf-8');
+console.log(data);
+
+//http
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.write('Hello from server');
+    res.end();
+});
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
+
+//path
+const path = require('path');
+
+const filePath = path.join('folder', 'file.txt');
+console.log(filePath);
+
+//os
+const os = require('os');
+
+console.log(os.platform());
+console.log(os.arch());
+console.log(os.cpus().length);
+
+//events
+const EventEmitter = require('events');
+
+const event = new EventEmitter();
+
+event.on('greet', () => {
+    console.log('Hello Event Triggered!');
+});
+
+event.emit('greet');
+
+//url
+
+const url = require('url');
+
+const myUrl = new URL('http://example.com/page?name=varsha');
+
+console.log(myUrl.hostname);
+console.log(myUrl.searchParams.get('name'));
+
+//crypto
+const crypto = require('crypto');
+
+const hash = crypto.createHash('sha256')
+                   .update('password123')
+                   .digest('hex');
+
+console.log(hash);
+
+//timer
+setTimeout(() => {
+    console.log('Runs after 2 seconds');
+}, 2000);
+
